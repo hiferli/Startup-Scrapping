bb2c9cef96c6981402f43ebefe88f81325084c6b
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-14 22:17:54 +0530

    Converting code to SlideCapture() Function

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello WOrld');
+import pageReader
+
+url_1 = r'https://startupdecks.co/bibles/zoe-financial/';
+url_2 = r'https://startupdecks.co/bibles/zestful/';
+
+pageReader.SlideCapture(url_1)
+pageReader.SlideCapture(url_2)
--- pageReader.py
+++ pageReader.py
@@ -6,24 +6,24 @@ import urllib.request
 # Directory for File Location 
 import DirectoryVariable
 
-url = r'https://startupdecks.co/bibles/zoe-financial/';
-# url = r'https://startupdecks.co/bibles/zestful/';
+def SlideCapture(url):
+    document = BeautifulSoup(requests.get(url).text , 'html.parser');
+    startupName = ((str(document.title.string)).split(' - ')[0])
 
-document = BeautifulSoup(requests.get(url).text , 'html.parser');
-startupName = ((str(document.title.string)).split(' - ')[0])
+    saveLocation = DirectoryVariable.fileLocation + startupName + "/"
 
-saveLocation = DirectoryVariable.fileLocation + startupName + "/"
+    if not os.path.isdir(saveLocation):
+        os.makedirs(saveLocation)
 
-if not os.path.isdir(saveLocation):
-   os.makedirs(saveLocation)
+    images = document.find_all(class_ = "grid-60 tablet-grid-50 mobile-grid-100 slide_image");
 
-images = document.find_all(class_ = "grid-60 tablet-grid-50 mobile-grid-100 slide_image");
+    index = 1;
+    for image in images:
+        link = image.contents[1]['src']
+        # urllib.request.urlretrieve(link, "C:/Users/ADMIN/Desktop/Startup Decks/Images/Sample_" + str(index) + ".jpg")
+        slideSaveDirectory = saveLocation + "/" + startupName + "-" + str(index) + ".jpg";
+        urllib.request.urlretrieve(link, slideSaveDirectory)
 
-index = 1;
-for image in images:
-    link = image.contents[1]['src']
-    # urllib.request.urlretrieve(link, "C:/Users/ADMIN/Desktop/Startup Decks/Images/Sample_" + str(index) + ".jpg")
-    slideSaveDirectory = saveLocation + "/" + startupName + "-" + str(index) + ".jpg";
-    urllib.request.urlretrieve(link, slideSaveDirectory)
+        index += 1;
 
-    index += 1;
+    print('All slide capture is successful for ' + startupName)
