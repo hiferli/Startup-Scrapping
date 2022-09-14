f5d5b8238d4c740e3e995dd9d2820f276e5c06d5
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-14 19:48:00 +0530

    Downloading Images from Web

--- pageReader.py
+++ pageReader.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib.request
 
 url = r'https://startupdecks.co/bibles/zoe-financial/';
 
@@ -8,7 +9,8 @@ document = BeautifulSoup(requests.get(url).text , 'html.parser');
 
 images = document.find_all(class_ = "grid-60 tablet-grid-50 mobile-grid-100 slide_image");
 
+index = 1;
 for image in images:
     link = image.contents[1]['src']
-    
-# print(document.prettify())  
+    urllib.request.urlretrieve(link, "C:/Users/ADMIN/Desktop/Startup Decks/Images/Sample_" + str(index) + ".jpg")
+    index += 1;
