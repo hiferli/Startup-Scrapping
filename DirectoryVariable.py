b42028a3980b0027a409dfe0d8a84e3347b50024
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-14 22:02:08 +0530

    Creating Directory Variable

---
+++ DirectoryVariable.py
@@ -0,0 +1,2 @@
+# Make sure to add the "/" in the end as well
+fileLocation = r'C:/Users/ADMIN/Desktop/Startup Decks/'
--- pageReader.py
+++ pageReader.py
@@ -3,16 +3,19 @@ from bs4 import BeautifulSoup
 import requests
 import urllib.request
 
+# Directory for File Location 
+import DirectoryVariable
+
 url = r'https://startupdecks.co/bibles/zoe-financial/';
 # url = r'https://startupdecks.co/bibles/zestful/';
 
 document = BeautifulSoup(requests.get(url).text , 'html.parser');
 startupName = ((str(document.title.string)).split(' - ')[0])
 
-fileLocation = r'C:/Users/ADMIN/Desktop/Startup Decks/' + startupName + "/"
+saveLocation = DirectoryVariable.fileLocation + startupName + "/"
 
-if not os.path.isdir(fileLocation):
-   os.makedirs(fileLocation)
+if not os.path.isdir(saveLocation):
+   os.makedirs(saveLocation)
 
 images = document.find_all(class_ = "grid-60 tablet-grid-50 mobile-grid-100 slide_image");
 
@@ -20,7 +23,7 @@ index = 1;
 for image in images:
     link = image.contents[1]['src']
     # urllib.request.urlretrieve(link, "C:/Users/ADMIN/Desktop/Startup Decks/Images/Sample_" + str(index) + ".jpg")
-    slideSaveDirectory = fileLocation + "/" + startupName + "-" + str(index) + ".jpg";
+    slideSaveDirectory = saveLocation + "/" + startupName + "-" + str(index) + ".jpg";
     urllib.request.urlretrieve(link, slideSaveDirectory)
 
     index += 1;
