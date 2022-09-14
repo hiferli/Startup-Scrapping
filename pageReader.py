bfea7b7ed5808919360e967fa642890d3a8b375d
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-14 19:29:46 +0530

    Getting Image Source Link from Files

--- pageReader.py
+++ pageReader.py
@@ -5,4 +5,10 @@ url = r'https://startupdecks.co/bibles/zoe-financial/';
 
 document = BeautifulSoup(requests.get(url).text , 'html.parser');
 
+
+images = document.find_all(class_ = "grid-60 tablet-grid-50 mobile-grid-100 slide_image");
+
+for image in images:
+    link = image.contents[1]['src']
+    
 # print(document.prettify())  
