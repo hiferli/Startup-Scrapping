93d195aedca3d9027a7b6fe61dfb849423fd8b6e
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-15 19:33:30 +0530

    Using Selenuim for HREF Extraction

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pageReader
+import DirectoryVariable
 
-url_1 = r'https://startupdecks.co/bibles/zoe-financial/';
-url_2 = r'https://startupdecks.co/bibles/zestful/';
+from selenium import webdriver    
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.service import Service
 
-pageReader.SlideCapture(url_1)
-pageReader.SlideCapture(url_2)
+chromedriver_autoinstaller.install()
+driver = webdriver.Chrome(service=Service())
+
+url = r'https://startupdecks.co/bibles/zoe-financial/';
+driver.get(url);
+
+driver.quit();
+
+# url_1 = r'https://startupdecks.co/bibles/zoe-financial/';
+# url_2 = r'https://startupdecks.co/bibles/zestful/';
+
+# pageReader.SlideCapture(url_1)
+# pageReader.SlideCapture(url_2)
