6c3bed1cf945be1be85a70fbc65751bb8dee7e07
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-15 23:30:02 +0530

    Using BS4 with Selenuim

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import DirectoryVariable
 from selenium import webdriver    
 import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 
 chromedriver_autoinstaller.install()
 driver = webdriver.Chrome(service=Service())
 
 url = r'https://startupdecks.co/bibles/zoe-financial/';
+
+# Selenium Work Here
 driver.get(url);
+
+# Using BeautifulSoup using Selenuim
+html = driver.page_source
+document = BeautifulSoup(html , "html.parser");
 
 driver.quit();
 
