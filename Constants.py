cb59d4a672adf300e302a229020e790105de2171
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-18 04:52:14 +0530

    Time in HH:MM:SS Format

--- Constants.py
+++ Constants.py
@@ -1,3 +1,13 @@
 url = r'https://startupdecks.co/decks/';
 startups_links = set();
 totalTime = 0
+
+def timeConvert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+     
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import Constants
 for link in startupURLs.GetStartups():
     pageReader.SlideCapture(link);
 
-print(f'All Slide Capture successful. Total Execution time is {Constants.totalTime}')
+print(f'All Slide Capture successful. Total Execution time is {round(Constants.totalTime , 3)} seconds ({Constants.timeConvert(round(Constants.totalTime , 3))})')
