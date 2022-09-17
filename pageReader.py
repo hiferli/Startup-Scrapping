45a644c7780389dccf6f651c49c830ce28f4ff47
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-18 04:14:27 +0530

    'No Folder when No Slides' modification

--- pageReader.py
+++ pageReader.py
@@ -9,33 +9,41 @@ import DirectoryVariable
 import Constants
 
 def SlideCapture(url):
-
     start_time = time.time()
-    
+
+    startup_slides_link = []
+
     document = BeautifulSoup(requests.get(url).text , 'html.parser');
+    
+    images = document.find_all(class_ = "grid-60 tablet-grid-50 mobile-grid-100 slide_image");
+    for image in images:
+        link = image.contents[1]['src']
+        if link != '':
+            startup_slides_link.append(link);
+    
     startupName = ((str(document.title.string)).split(' - ')[0])
+    if(len(startup_slides_link) == 0):
+        print(f"No slides found for {startupName}. Heading towards the next startup...")
+        return
 
     saveLocation = DirectoryVariable.fileLocation + startupName + "/"
 
     if not os.path.isdir(saveLocation):
         os.makedirs(saveLocation)
 
-    images = document.find_all(class_ = "grid-60 tablet-grid-50 mobile-grid-100 slide_image");
-
     index = 1;
-    for image in images:
-        link = image.contents[1]['src']
-        
-        if link != '':
-            # urllib.request.urlretrieve(link, "C:/Users/ADMIN/Desktop/Startup Decks/Images/Sample_" + str(index) + ".jpg")
-            slideSaveDirectory = saveLocation + "/" + startupName + "-" + str(index) + ".jpg";
-            try:
-                urllib.request.urlretrieve(link, slideSaveDirectory)
-            except:
-                pass;
+    for link in startup_slides_link:
+        # urllib.request.urlretrieve(link, "C:/Users/ADMIN/Desktop/Startup Decks/Images/Sample_" + str(index) + ".jpg")
+        slideSaveDirectory = saveLocation + "/" + startupName + "-" + str(index) + ".jpg";
+        try:
+            urllib.request.urlretrieve(link, slideSaveDirectory)
+        except:
+            pass;
                 
         index += 1;
+
 
     print(f'All Slides Captured for {startupName}.')
     Constants.totalTime = Constants.totalTime + (time.time() - start_time) 
     
+SlideCapture('https://startupdecks.co/bibles/sorare/')
