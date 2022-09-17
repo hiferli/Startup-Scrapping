3ef8086d48a4a2c781841f42475e81b6269aff7f
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-18 04:59:03 +0530

    Separate Folder for Slides

--- DirectoryVariable.py
+++ DirectoryVariable.py
@@ -1,5 +1,7 @@
 # Make sure to add the "/" in the end as well
-fileLocation = r'C:/Users/ADMIN/Desktop/Startup Decks/'
+fileLocation = r'C:/Users/ADMIN/Desktop/Startup Decks/Slides/'
+
+# Not here. Thank You
 driverPath = r'C:\Users\ADMIN\Desktop\Startup Decks\Drivers\chromedriver.exe'
 
 sleepDuration = 5;
