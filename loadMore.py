ff9af3d8d186ad09983ace155f892e91d14a676c
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-18 02:32:45 +0530

    Creating Load More as Function

--- loadMore.py
+++ loadMore.py
@@ -7,21 +7,23 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
+def getMoreStartups():
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome(service=Service())
 
-chromedriver_autoinstaller.install()
-driver = webdriver.Chrome(service=Service())
+    driver.get(DirectoryVariable.url);
 
-driver.get(DirectoryVariable.url);
+    time.sleep(DirectoryVariable.sleepDuration)
 
-time.sleep(DirectoryVariable.sleepDuration)
+    # Using BeautifulSoup using Selenuim
+    html = driver.page_source.encode('utf-8').strip()
+    document = BeautifulSoup(html , "html.parser");
 
-# Using BeautifulSoup using Selenuim
-html = driver.page_source.encode('utf-8').strip()
-document = BeautifulSoup(html , "html.parser");
+    # print(document.find(class_ = 'loadmore'))
+    (driver.find_element(By.CLASS_NAME , 'loadmore').click())
+    time.sleep(DirectoryVariable.sleepDuration)
+    document = driver.page_source.encode('utf-8').strip()
 
-# print(document.find(class_ = 'loadmore'))
-(driver.find_element(By.CLASS_NAME , 'loadmore').click())
-time.sleep(DirectoryVariable.sleepDuration)
-print(driver.page_source.encode('utf-8').strip())
+    driver.quit();
 
-driver.quit();
+    return document;
