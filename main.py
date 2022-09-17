492d44eb6f91a34f5dc2db2ec2eb9ded956a687f
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-18 01:13:49 +0530

    Extracting Startup Link from Page

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
+from xml.dom.minidom import DocumentFragment
 import pageReader
 import DirectoryVariable
 
-from selenium import webdriver    
+from selenium import webdriver   
+from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
+import time
 
 chromedriver_autoinstaller.install()
 driver = webdriver.Chrome(service=Service())
 
-url = r'https://startupdecks.co/bibles/zoe-financial/';
+url = r'https://startupdecks.co/decks/';
 
 # Selenium Work Here
 driver.get(url);
 
+
+time.sleep(5)
+
 # Using BeautifulSoup using Selenuim
-html = driver.page_source
+html = driver.page_source.encode('utf-8').strip()
 document = BeautifulSoup(html , "html.parser");
+
+# Main code here
+startup_cards = document.find_all(class_ = 'deck_list_item');
+# print((startup_cards));
+
+for startup in startup_cards:
+    print(startup.find('a').get('href'))
 
 driver.quit();
 
