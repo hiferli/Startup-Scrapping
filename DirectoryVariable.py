43e7229fccee3ec3d64ca396a06b51cf69d55cba
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-18 17:05:05 +0530

    Getting User CWD for Folder Reference

--- DirectoryVariable.py
+++ DirectoryVariable.py
@@ -1,8 +1,16 @@
+import os
+
+print(os.getcwd())
+
+fileLocation = os.path.join(os.getcwd() , r'Slides')
+if not os.path.exists(fileLocation):
+    os.makedirs(fileLocation)
+
 # Make sure to add the "/" in the end as well
-fileLocation = r'C:/Users/ADMIN/Desktop/Startup Decks/Slides/'
+# fileLocation = r'C:/Users/ADMIN/Desktop/Startup Decks/Slides/'
 
 # Not here. Thank You
-driverPath = r'C:\Users\ADMIN\Desktop\Startup Decks\Drivers\chromedriver.exe'
+# driverPath = r'C:\Users\ADMIN\Desktop\Startup Decks\Drivers\chromedriver.exe'
 
 sleepDuration = 5;
-numberOfStartups = 20;
+numberOfStartups = 5;
